3e20541fccead5a30900c8a1907228da3e35a7b4
RohanAdityaRam <RohanAdityaRam@IN-PC-00558>
2024-01-24 16:19:58 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using electronicsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+
+namespace electronicsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = "http://35.238.57.3:8080/product";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+                    List<UrlContent> jsonData = JsonConvert.DeserializeObject<List<UrlContent>>(jsonContent);
+                    return View(jsonData);
+                }
+                else
+                {
+                    // Handle error, e.g., return a view with an error message
+                    return View("Error");
+                }
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/UrlContent.cs
@@ -0,0 +1,11 @@
+﻿namespace electronicsApp.Models
+{
+    internal class UrlContent
+    {
+        public string id { get; set; }
+        public int noofproduct { get; set; }
+        public double price { get; set; }
+        public string pname { get; set; }
+        public string batchno { get; set; }
+    }
+}
